54d1937367a21b92dc0236f95197499e19f1b5f6
Fabricio <fabriciolak12@gmail.com>
2023-11-25 00:53:28 -0300

    chore: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Heading } from "@/components/heading";
+import { Header } from "@/components/header"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Heading>
-        Popular Recipes
-      </Heading>
-    </main>
+    <div>
+      <Header />
+    </div>
   )
 }
