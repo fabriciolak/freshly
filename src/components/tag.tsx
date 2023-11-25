c07774ab25612730efee40ec37ec9ac7dcd01479
Fabricio <fabriciolak12@gmail.com>
2023-11-25 00:47:43 -0300

    feat: made tag component

---
+++ src/components/tag.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text } from './text'
+import { tw } from '@/utils/ts-merge'
+import { spaceGrotesk } from '@/styles/fonts'
+
+interface TagProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const Tag = React.forwardRef<HTMLDivElement, TagProps>(({ children, className, ...props }, ref) => {
+  return (
+    <div ref={ref} {...props} className={tw(['flex gap-2 justify-center items-center py-1 px-2 rounded-full bg-white/10', spaceGrotesk.className, className])}>
+      {children}
+    </div>
+  )
+})
+
+Tag.displayName = 'Tag'
